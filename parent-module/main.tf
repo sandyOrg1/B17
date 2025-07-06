a0dbb70a48cb84da36d47ccb788bc88aa5943757
sandeepsitm91 <sandeepsitm91@gmail.com>
2025-07-06 22:43:38 +0530

    Feature/sandy2 (#2)
    * one rg
    
    * second rg

--- parent-module/main.tf
+++ parent-module/main.tf
@@ -2,4 +2,8 @@ module "rg1" {
     source = "../child-module/azurerm_resource_group"
     resource_group_name = "sandy-rg1"
     location = "West Us2"
+module "rg2" {
+    source = "../child-module/azurerm_resource_group"
+    resource_group_name = "sandy-rg2"
+    location = "West Us2"
 }
